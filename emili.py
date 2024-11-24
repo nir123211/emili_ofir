875faa0162afbaa3fde788371b1d28743ef72ad9
User <ememimi5@gmail.com>
2024-11-24 19:11:22 +0200

    commit my story

---
+++ emili.py
@@ -0,0 +1,2 @@
+def print_funny_story():
+    print("I was once jogging with my mother and saw a huge rat and a policeman wanted to save us, but when we ran past him he scared us and we screamed at the top of our lungs")
